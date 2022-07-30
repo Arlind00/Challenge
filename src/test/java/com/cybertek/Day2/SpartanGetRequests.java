0fa70ce8194ab9d39b83a82347bc292412143940
arlind00 <aarlind@live.com>
2022-07-30 12:48:13 +0200

    day2 - 2nd commit

--- src/test/java/com/cybertek/Day2/SpartanGetRequests.java
+++ src/test/java/com/cybertek/Day2/SpartanGetRequests.java
@@ -26,8 +26,8 @@ public class SpartanGetRequests {
     @Test
     public void test1() {
         Response response = RestAssured.given().accept(ContentType.JSON)      // giving the Header type (we store everything in response variable)
-                .when()                                                       //  optional to use
-                .get(baseUrl + "/api/spartans");                           // base URL + endpoint
+                .when()                                                       // optional to use
+                .get(baseUrl + "/api/spartans");                        // base URL + endpoint
 
 
         // printing status code from response object
@@ -49,9 +49,6 @@ public class SpartanGetRequests {
 
 
 
-
-
-
      /*
         Given accept header is application/json
         When users sends a get request to /api/spartans/3
@@ -62,7 +59,7 @@ public class SpartanGetRequests {
 
     @DisplayName("GET one spartan / api/spartans/3 and verify")
     @Test
-    public void test2(){
+    public void test2() {
 
         Response response = RestAssured.given().accept(ContentType.JSON)                   // giving the Header type (we store everything in response variable)
                 .when()
@@ -77,7 +74,45 @@ public class SpartanGetRequests {
 
         // verify json body contains Fidole
         Assertions.assertTrue(response.body().asString().contains("Fidole"));              // converts result at response into string and checks if it contains
-                                                                                           // the result Fidole ( not a good (not specific enough) way to check)
+        // the result Fidole ( not a good (not specific enough) way to check)
+    }
 
+
+
+/*
+        Given no headers provided
+        When Users sends GET request to /api/hello
+        Then response status code should be 200
+        And Content type header should be “text/plain;charset=UTF-8”
+        And header should contain date
+        And Content-Length should be 17
+        And body should be “Hello from Sparta"
+        */
+
+    @DisplayName("Get  request to /api/hello")
+    @Test
+    public void test3(){
+
+        Response response = RestAssured.when().get(baseUrl + "/api/hello");
+
+        // verify status code is 200
+        Assertions.assertEquals(200, response.statusCode());
+
+        // verify response content type
+        Assertions.assertEquals("text/plain;charset=UTF-8",response.contentType() );
+
+        // verify we have headers named date
+        Assertions.assertTrue(response.headers().hasHeaderWithName("Date"));
+
+        // we select one specific header from that we want to check
+        System.out.println("response.header(\"Content-Length\") = " + response.header("Content-Length"));
+        System.out.println("response.header(\"Date\") =: " + response.header("Date"));
+
+
+        // verify content length is equal 17
+        Assertions.assertEquals("17",response.header("Content-Length"));
+
+        // verify body is “Hello from Sparta"
+        Assertions.assertEquals( "Hello from Sparta", response.body().asString());
     }
 }
