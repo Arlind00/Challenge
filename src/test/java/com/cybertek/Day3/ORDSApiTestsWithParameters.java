a9e01fd4a1a297999f68e3aa3c28d846246abcb2
arlind00 <aarlind@live.com>
2022-08-02 15:33:52 +0200

    day3 - 6th commit

--- src/test/java/com/cybertek/Day3/ORDSApiTestsWithParameters.java
+++ src/test/java/com/cybertek/Day3/ORDSApiTestsWithParameters.java
@@ -1,6 +1,10 @@
 package com.cybertek.Day3;
 
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.*;                  // to be able to import static methods
 import static org.junit.jupiter.api.Assertions.*;            // to be able to import static methods
@@ -12,19 +16,40 @@ public class ORDSApiTestsWithParameters {
     // is executed for entire class not just before each scenario as Hooks did, saves baseURL inside this variable
     @BeforeAll
     public static void init() {
-        baseURI = "http://52.91.45.47:1000/hr/ords/hr";                        // base URL
+        baseURI = "http://52.91.45.47:1000/ords/hr";                        // base URL
     }
 
 
 
     /*
         Given accept type is Json
-        And parameters: q = {"region_id":2}
+        And parameters: q = {\"region_id\":2}
         When users sends a GET request to "/countries"
         Then status code is 200
         And Content type is application/json
         And Payload should contain "United States of America"
      */
+
+    @DisplayName("GET request to /countires with Query Param ")
+    @Test
+    public void test1(){
+        Response response = given().accept(ContentType.JSON)
+                .and().queryParam("q","{\"region_id\":2}")
+                .log().all()
+                .when()
+                .get("/countries");
+
+        assertEquals(200, response.statusCode());
+        assertEquals("application/json", response.header("Content-Type"));      // checks for the value of specific header
+        assertTrue(response.headers().hasHeaderWithName("\"Content-Type\""));       // checks if the specific header exists
+        assertTrue(response.body().asString().contains("United States of America"));
+
+        response.prettyPrint();
+
+
+
+
+    }
 
 
 }
