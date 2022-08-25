2dc7e28f565d3e617ff97e752e4a0e4f243198a4
arlind00 <aarlind@live.com>
2022-08-25 08:44:13 +0200

    day7- 2nd commit

--- src/test/java/com/cybertek/Day7/SpartanPostRequestDemo_1.java
+++ src/test/java/com/cybertek/Day7/SpartanPostRequestDemo_1.java
@@ -3,9 +3,12 @@ package com.cybertek.Day7;
 import com.cybertek.Utilities.SpartanTestBase;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -58,6 +61,43 @@ public class SpartanPostRequestDemo_1 extends SpartanTestBase {
         assertThat(response.path("data.name"), is("Mike"));
         assertThat(response.path("data.gender"), is("Male"));
         assertThat(response.path("data.phone"), is(9845315689L));
+    }
+
+
+
+
+
+    @DisplayName("POST with Map to JSON")
+    @Test
+    public void postMethod2(){
+
+        // create a map to keep request json body information
+        Map<String,Object> requestJsonMap = new LinkedHashMap<>();              // object because value could be anything, linkedHashMap for order
+        requestJsonMap.put("name","Mike");
+        requestJsonMap.put("gender","Male");
+        requestJsonMap.put("phone",9845315689L);
+
+
+        Response response = given().accept(ContentType.JSON).and()          // what we are asking from api which is JSON response
+                .contentType(ContentType.JSON)                              // what we are sending to api, which is JSON also
+                .body(requestJsonMap)                                       // serilization done implicitly for PATCH, PUT and POST
+                .log().all()
+                .when()
+                .post("/api/spartans");                               // till today, we used GET, first time we are using POST
+
+
+        assertThat(response.statusCode(),is(201));
+        assertThat(response.contentType(),is("application/json"));
+
+        String expectedResponseMessage = "A Spartan is Born!";                            // expected response message
+
+        // Hemcrest assertions
+        assertThat(response.path("success"), is(expectedResponseMessage));          // we use path method to retrieve information
+        assertThat(response.path("data.name"), is("Mike"));
+        assertThat(response.path("data.gender"), is("Male"));
+        assertThat(response.path("data.phone"), is(9845315689L));
+
+        response.prettyPrint();
     }
 }
 
